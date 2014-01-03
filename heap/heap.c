90af031adc51bb4230dd459959afca5e20ea1cc3
hegek87 <hegelandka@vcu.edu>
2014-01-03 16:12:53 -0500

    Mostly done with heap

--- heap/heap.c
+++ heap/heap.c
@@ -16,35 +16,89 @@ void swap(int *x, int *y){
 	*y = temp;
 }
 
-void max_heapify(int *heap_data, int initial){
+void heapify(int *heap_data, int initial, int heap_size){
 	int left = LEFT(initial), right = RIGHT(initial);
 	int temp = *(heap_data + initial);
 	int left_key = *(heap_data+left), right_key = *(heap_data+right);
 	int largest = (left_key HEAP_TYPE temp) ? left : initial;
-	largest = (*(heap_data + largest)HEAP_TYPE right_key) ? largest : right;
-	//BAD TEMP FIX FOR THE NIGHT (WILL FIX TOMORROW)
-	if(largest > 4) return;
+	largest = (*(heap_data + largest)HEAP_TYPE right_key) ? largest :right;
+	if(largest > heap_size) return;
 	if(largest != initial){
 		swap((heap_data + initial), (heap_data + largest));
-		max_heapify(heap_data, largest);
+		heapify(heap_data, largest, heap_size);
 	}
 }
 
-struct heap *build_max_heap(int *heap_data, int heap_size);
-void max_heap_insert();
-int heap_extract_max(struct heap *);
+struct heap *build_heap(int *heap_data, int heap_size){
+	struct heap *new_heap = malloc(sizeof(struct heap));
+	new_heap->heap_size = new_heap->data_length = heap_size;
+	int i = heap_size / 2;
+	while(i >= 0){
+		//printf("hapify(heap_data, %d, %d);\n", i, heap_size);
+		heapify(heap_data, i, heap_size);
+		--i;
+	}
+	new_heap->data = heap_data;
+	return new_heap;
+}
+
+void heap_insert();
+
+int heap_extract_top(struct heap *h){
+	if(h->heap_size < 0){
+		perror("Heap underflow\n");
+	}
+	int max = *(h->data);
+	*(h->data) = *(h->data + h->heap_size);
+	h->heap_size -= 1;
+	heapify(h->data, 0, h->heap_size);
+	return max;
+}
+
 void heap_increase_key(struct heap *, int, int);
-int heap_max(struct heap *);
+
+int heap_top(struct heap *heap){
+	return *(heap->data);
+}
+
+void print_heap(struct heap *heap){
+	int *p = heap->data, i = 0;
+	for( ; i++ <= heap->heap_size; ++p){
+		printf("%d ", *p);
+	}
+	printf("\n");
+}
+
+void heapsort(int to_sort[], int length){
+	struct heap *h = build_heap(to_sort, length-1);
+	int i = h->heap_size+1;
+	while(i >= 1){
+		swap(h->data, (h->data + i-1));
+		h->heap_size -= 1;
+		heapify(h->data, 0, h->heap_size);
+		--i;
+	}
+}
 
 int main(void){
-	int *x = calloc(sizeof(int), 4);
-	//malloc(4 * sizeof(int));
-	int a = 10, b = 14, c = 9;
-	*(x+1) = a, *(x+2) = b, *(x+3) = c;
-	printf("Heap data:\n\tPARENT: %d\n\tLEFT: %d\n\tRIGHT: %d\n",
-		*(x+1), *(x+2), *(x+3));
-	max_heapify(x, 1);
-	printf("Heap data:\n\tPARENT: %d\n\tLEFT: %d\n\tRIGHT: %d\n",
-		*(x+1), *(x+2), *(x+3));
+	int *x;
+	int arr[13] = {5,2, 4,12,42,5,25,63,2,6,24,13,2};
+	x=arr;
+	int i = 0;
+	for(i = 0; i < 13; ++i){
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	struct heap *t = build_heap(x, 12);
+	print_heap(t);
+	printf("Top element: %d\n", heap_top(t));
+	int arr1[10] = {10,9,8,7,6,5,4,3,2,1};
+	struct heap *t1 = build_heap(arr1, 9);
+	//heapsort(arr1, 10);
+	
+	for(i = t1->heap_size;i >= 0; --i){
+		printf("%d\n", heap_extract_top(t1));
+	}
+	//printf("\n");
 	return 0;
 }
--- heap/heap.h
+++ heap/heap.h
@@ -1,25 +1,30 @@
 #ifndef HEAP_H
 #define HEAP_H
-#define HEAP_TYPE MIN
+#define HEAP_TYPE MAX
 #define MAX >
 #define MIN <
 
-#define PARENT(X) ( (X) >> 1)
-#define LEFT(X) ((X) << 1)
-#define RIGHT(X) (((X) << 1) + 0x1)
+#define PARENT(X) ( ((X)-0x1) >> 1)
+#define LEFT(X) ((X) << 1) + 0x1
+#define RIGHT(X) (((X) << 1) + 0x2)
 
+//0<=heap_size<=data_length
 struct heap{
 	int heap_size;
+	int data_length;
 	int *data;
 };
 
 int max(int, int, int);
 
-void max_heapify(int *, int);
-struct heap *build_max_heap(int *, int);
+void heapify(int *, int, int);
+struct heap *build_heap(int *, int);
 void max_heap_insert();
 int heap_extract_max(struct heap *);
 void heap_increase_key(struct heap *, int, int);
 int heap_max(struct heap *);
+void print_heap(struct heap *);
+
+void heapsort(int[], int);
 
 #endif
